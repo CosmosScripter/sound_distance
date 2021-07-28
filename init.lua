5d2a693d5508b29d177cc87cf9f10605f0ef6fe3
CosmosScripter <50247544+CosmosScripter@users.noreply.github.com>
2021-07-28 19:57:40 -0300

    Added sound speed barrier
    Objects moving above sound speed will have a sound delay, sonic booms were also added.
    
    force_sound_speed can be used to force the effects of the sound barrier even with slow objects.

--- init.lua
+++ init.lua
@@ -1,7 +1,27 @@
 --Fun fact: the "Doppler Effect" is physics law about waves, according to it, basically waves change depending on their distance.
 --The Doppler Effect is what makes us able to tell if the sound origin is getting closer or further.
 
---Possible update: Objects moving above sound speed?
+local sonic_boom = {
+	physical = false,
+	timer = 0,
+	visual = "sprite",
+	visual_size = {x=0.5, y=0.5},
+	textures = {"sonic_boom.png"},
+	lastpos= {},
+	collisionbox = {0, 0, 0, 0, 0, 0},
+}
+sonic_boom.on_step = function(self, dtime, pos)--Sonic boom expanding
+    sound_distance_play(self.object:get_pos(), "tnt_explode", 1, 32)--Sonic boom noise
+    self.timer = (self.timer or 0) + dtime
+    local size_x = self.object:get_properties().visual_size.x
+    local size_y = self.object:get_properties().visual_size.y
+    self.object:set_properties({visual_size = {x=size_x + 0.1, y=size_y + 0.1}})
+    if self.timer > 1 then--Fun fact: the first time this was tested, the time limit was too long, so it became a Detroit Smash
+        self.object:remove()
+    end
+end
+
+minetest.register_entity("sound_distance:sonic_boom", sonic_boom)
 
 --Note: "a" stands for the target position, while "b" is the origin position
 local get_distance = function(a, b) --This function is from mobs redo
@@ -100,7 +120,7 @@ local check_movement = function(pos, yaw, player)--For now this kind of works, f
     end
 end
 
-function sound_distance_play(pos, sound, sound_gain, distance, moving, yaw)
+function sound_distance_play(pos, sound, sound_gain, distance, moving, yaw, vel, force_sound_speed)
     if not pos or not sound or not distance then return end
     if not sound_gain then sound_gain = 1 end
 	for _,player in ipairs(minetest.get_connected_players()) do
@@ -111,7 +131,12 @@ function sound_distance_play(pos, sound, sound_gain, distance, moving, yaw)
         if not doppler then return end
         if not player then return end--Avoid glitching incase player leaves
         if moving == true and not yaw then yaw = 0 end
+        if not vel then vel = {x = 10, y = 0, z = 10} end
+        local actual_vel = math.ceil(vel.x * vel.x + vel.z * vel.z) ^ 0.5--Converts a vector into an approximate number
+        if force_sound_speed == true then actual_vel = 340 end
+        if actual_vel >= 340 and actual_vel <= 342 then minetest.add_entity(pos, "sound_distance:sonic_boom") end
         if moving == true and check_movement(pos, yaw, player) == true then--Thanks to Astrobe I've noticed the mod was missing pitch changes
+            if actual_vel >= 340 then return end--Object is moving above or at sound speed, so only those behind the object can hear it
             --minetest.chat_send_player(player:get_player_name(), "working pitch change")
             if doppler == 0 then
                 minetest.sound_play(sound, {gain = 0.1, pitch = 2, to_player = player:get_player_name()})--Higher pitch incase the object is moving and aproaching
@@ -123,19 +148,38 @@ function sound_distance_play(pos, sound, sound_gain, distance, moving, yaw)
                 end
             end
         else
-            if doppler == 0 then
-                minetest.sound_play(sound, {gain = 0.1, to_player = player:get_player_name()})
+            if actual_vel > 340 then--If above sound speed, the sound will be delayed
+                local sound_timing = actual_vel - 340
+                if not sound_timing then return end
+                minetest.after(sound_timing, function()
+                    if doppler == 0 then
+                        minetest.sound_play(sound, {gain = 0.1, to_player = player:get_player_name()})
+                    else
+                        if sound_gain*(doppler/10) > sound_gain then
+                            minetest.sound_play(sound, {gain = sound_gain, to_player = player:get_player_name()})
+                        else
+                            minetest.sound_play(sound, {gain = sound_gain*(doppler/10), to_player = player:get_player_name()})
+                        end
+                    end
+                end)
             else
-                if sound_gain*(doppler/10) > sound_gain then--This avoids a possible ear exploder incase the sound has a huge hear distance for example
-                    minetest.sound_play(sound, {gain = sound_gain, to_player = player:get_player_name()})--The sound is played only for that specific player
+                if doppler == 0 then
+                    minetest.sound_play(sound, {gain = 0.1, to_player = player:get_player_name()})
                 else
-                    minetest.sound_play(sound, {gain = sound_gain*(doppler/10), to_player = player:get_player_name()})
+                    if sound_gain*(doppler/10) > sound_gain then--This avoids a possible ear exploder incase the sound has a huge hear distance for example
+                        minetest.sound_play(sound, {gain = sound_gain, to_player = player:get_player_name()})--The sound is played only for that specific player
+                    else
+                        minetest.sound_play(sound, {gain = sound_gain*(doppler/10), to_player = player:get_player_name()})
+                    end
                 end
             end
         end
     end
 end
 
+--Testing stuff
+
+--Node
 --[[minetest.register_node("sound_distance:test_node", {
 	description = "Sound distance test",
 	tiles = {"default_steel_block.png"},
@@ -154,6 +198,7 @@ end
 	end,
 })
 
+--Moving sound emitter
 local test_entity = {
 	physical = false,
 	timer = 0,
@@ -164,15 +209,16 @@ local test_entity = {
 	collisionbox = {0, 0, 0, 0, 0, 0},
 }
 test_entity.on_step = function(self, dtime, pos)--Moving sound source
-    sound_distance_play(self.object:get_pos(), "default_place_node", 10, 32, true, self.object:get_yaw())--A repeating short sound
+    sound_distance_play(self.object:get_pos(), "default_place_node", 10, 100, true, self.object:get_yaw(), self.object:get_velocity())--A repeating short sound
     self.timer = (self.timer or 0) + dtime
-    if self.timer > 10 then
+    if self.timer > 40 then
         self.object:remove()
     end
 end
 
 minetest.register_entity("sound_distance:test_entity", test_entity)
 
+--Below sound speed
 minetest.register_craftitem("sound_distance:test_item", {
 	inventory_image = "default_steel_block.png",
 	description = "Moving sound source (TEST)",
@@ -186,6 +232,58 @@ minetest.register_craftitem("sound_distance:test_item", {
 			local obj = minetest.add_entity(pos, "sound_distance:test_entity")
 			if obj then
 				obj:setvelocity({x=dir.x * 12, y=dir.y * 12, z=dir.z * 12})
+				obj:setyaw(yaw)
+				local ent = obj:get_luaentity()
+				if ent then
+					ent.player = ent.player or user
+			itemstack = ""
+				end
+			end
+		end
+		return itemstack
+    end,
+})
+
+--Above sound speed (delayed sound)
+minetest.register_craftitem("sound_distance:ultrasonic_item", {
+	inventory_image = "default_steel_block.png",
+	description = "Ultrasonic sound source (TEST)",
+    stack_max = 1,
+    on_use = function (itemstack, user, pointed_thing)
+		local pos = user:getpos()
+		local dir = user:get_look_dir()
+		local yaw = user:get_look_horizontal()
+		if pos and dir and yaw then
+			pos.y = pos.y + 1.6
+			local obj = minetest.add_entity(pos, "sound_distance:test_entity")
+			if obj then
+				obj:setvelocity({x=dir.x * 345, y=dir.y * 345, z=dir.z * 345})
+				obj:setyaw(yaw)
+				local ent = obj:get_luaentity()
+				if ent then
+					ent.player = ent.player or user
+			itemstack = ""
+				end
+			end
+		end
+		return itemstack
+    end,
+})
+
+--At sound speed (sonic boom and only people behind the object can hear it)
+minetest.register_craftitem("sound_distance:sound_speed_item", {
+	inventory_image = "default_steel_block.png",
+	description = "Sound speed sound source (TEST)",
+    stack_max = 1,
+    on_use = function (itemstack, user, pointed_thing)
+		local pos = user:getpos()
+		local dir = user:get_look_dir()
+		local yaw = user:get_look_horizontal()
+		if pos and dir and yaw then
+			pos.y = pos.y + 1.6
+			local obj = minetest.add_entity(pos, "sound_distance:test_entity")
+			if obj then
+				obj:setvelocity({x=dir.x * 340, y=dir.y * 340, z=dir.z * 340})
 				obj:setyaw(yaw)
 				local ent = obj:get_luaentity()
 				if ent then
